208df6a7bb69f73205a09a972dd6c3f88b148380
tusiartihand22 <47565755+tusiartihand22@users.noreply.github.com>
2019-03-30 10:03:15 +0700

    mandatory function for simple calc

---
+++ calculator.js
@@ -0,0 +1,40 @@
+function button1(){
+    document.getElementById("screen").value += 1;
+}
+function button2(){
+    document.getElementById("screen").value += 2;
+}
+function button3(){
+    document.getElementById("screen").value += 3;
+}
+function button4(){
+    document.getElementById("screen").value += 4;
+}
+function button5(){
+    document.getElementById("screen").value += 5;
+}
+function button6(){
+    document.getElementById("screen").value += 6;
+}
+function button7(){
+    document.getElementById("screen").value += 7;
+}
+function button8(){
+    document.getElementById("screen").value += 8;
+}
+function button9(){
+    document.getElementById("screen").value += 9;
+}
+function button0(){
+    document.getElementById("screen").value += 0;
+}
+function buttonPlus(){
+    document.getElementById("screen").value += "+";
+}
+function buttonMinus(){
+    document.getElementById("screen").value += "-";
+}
+function buttonEqual(){
+    let result = document.getElementById("screen").innerText;
+    console.log(result);
+}
